8a426555d7681ff214430d5a15a6640da1f3d4ee
Vignesh-1704 <vigneshmanikavasagam17@gmail.com>
2023-07-03 17:30:29 +0530

    Add. Added the Code for Accessing Individual Element from JSON String using JsonNode

--- src/main/java/com/m2p/JacksonAnnotations/ObjectMapperDeSerialization.java
+++ src/main/java/com/m2p/JacksonAnnotations/ObjectMapperDeSerialization.java
@@ -1,15 +1,30 @@
 package com.m2p.JacksonAnnotations;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.m2p.JacksonAnnotations.entity.Employee;
+import org.apache.catalina.webresources.StandardRoot;
+
+import java.io.File;
+import java.io.IOException;
 
 public class ObjectMapperDeSerialization {
-    public static void main(String[] args) throws JsonProcessingException {
+    public static void main(String[] args) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
         String employeeJson = "{\"name\":\"Vignesh\",\"city\":\"chennai\",\"department\":\"SAAS\",\"designation\":\"SDE1\"}";
         Employee employee = objectMapper.readValue(employeeJson, Employee.class);
         // Run in Debug Mode to check the employee object
         System.out.println(employee.getName());
+
+        //To read a particular value in JSON String without converting it to Java Objects
+        JsonNode jsonNode = objectMapper.readTree(employeeJson);
+
+        String empName = jsonNode.get("name").asText();
+        String empCity = jsonNode.get("city").asText();
+        String empDept = jsonNode.get("department").asText();
+        String empDesignation = jsonNode.get("designation").asText();
+
+        objectMapper.writeValue(new File("target/employeeNode.txt"),empName + "," + empCity + "," +  empDept + "," + empDesignation);
     }
 }
--- src/main/java/com/m2p/JacksonAnnotations/ObjectMapperSerialization.java
+++ src/main/java/com/m2p/JacksonAnnotations/ObjectMapperSerialization.java
@@ -7,10 +7,10 @@ import java.io.File;
 import java.io.IOException;
 
 public class ObjectMapperSerialization {
-//    public static void main(String[] args) throws IOException {
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        Employee employee = new Employee("Vignesh","chennai","SAAS","SDE1");
-//        objectMapper.writeValue(new File("target/emp.json"),employee);
-//    }
+    public static void main(String[] args) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Employee employee = new Employee("Vignesh","chennai","SAAS","SDE1");
+        objectMapper.writeValue(new File("target/emp.json"),employee);
+    }
 }
 
